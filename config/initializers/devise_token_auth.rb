7901b0c7aa0c62e6a5ff3f6a8993aae6a9cf9394
Judah Frankel <judahfrankel@gmail.com>
2016-03-31 15:38:44 +0800

    Testing...

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,4 +1,6 @@
 DeviseTokenAuth.setup do |config|
+
+  config.remember_for = 20.years
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
